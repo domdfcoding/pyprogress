ced99ed735f841d7aa92ef37e604398d3f761ea4
Dominic Davis-Foster <domdfcoding@gmail.com>
2019-04-01 09:44:07 +0100

    Added handler for HTTPError
    If an HTTPError is raised (such as for 403 Forbidden) the error code and reason will be printed

--- progress.py
+++ progress.py
@@ -1,13 +1,13 @@
 import time
 import sys
 from urllib.request import urlretrieve
-from urllib.error import URLError
+from urllib.error import URLError, HTTPError
 
 def progress(downloaded, block_size, total_size):
     global download_size
     download_size = total_size
     completed = int(downloaded / (total_size//block_size) * 100)
-    sys.stdout.write("\r|" + "█" * completed + " " * (100-completed) + "|{}%".format(completed))
+    sys.stdout.write(f"\r|{'█' * completed}{' ' * (100-completed)}{completed}%")
 
 def download_bar(url,filename):
     start = time.time()
@@ -16,17 +16,20 @@ def download_bar(url,filename):
     except URLError:
         sys.stderr.write("The URL is invalid.\n")
         exit(0)
+    except HTTPError as e:
+        sys.stderr.write(f"Error: {e.code} {e.reason}\n")
+        exit(0)
     except Exception as e:
         sys.stderr.write("An error occured\n")
         sys.stderr.write(type(e),e)
         exit(0)
     end = time.time()
-    print("\n",end="")
+    sys.stdout.write("\n")
     time_taken = end - start
     download_speed = round(download_size/(time_taken*1024),2)
     if download_speed >= 1024:
-        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed/1024) + " MB/s)" + "\n")
+        sys.stdout.write(f"Downloaded in {(round(time_taken, 2))} s ({download_speed/1024}  MB/s)\n")
     if download_speed < 1024:
-        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed) + " kB/s)" + "\n")
+        sys.stdout.write(f"Downloaded in {(round(time_taken, 2))} s ({download_speed}  kB/s)\n")
     if download_speed < 1:
-        sys.stdout.write("Downloaded in " + str(round(time_taken,2)) + "s (" + str(download_speed*1024) + " B/s)" + "\n")
+        sys.stdout.write(f"Downloaded in {(round(time_taken, 2))} s ({download_speed*1024}  B/s)\n")
